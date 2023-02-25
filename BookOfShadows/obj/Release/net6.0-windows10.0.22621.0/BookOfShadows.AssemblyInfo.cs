e594c736187aca21c54e5b864eb605b2bc699fa0
Kyle Martinez <elyk17@yahoo.com>
2023-02-24 17:59:55 -0700

    made changes to builds

--- BookOfShadows/obj/Release/net6.0-windows10.0.22621.0/BookOfShadows.AssemblyInfo.cs
+++ BookOfShadows/obj/Release/net6.0-windows10.0.22621.0/BookOfShadows.AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyProductAttribute("BookOfShadows")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookOfShadows")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/elyk17/bos")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://elyk17.github.io")]
 [assembly: System.Resources.NeutralResourcesLanguageAttribute("en-US")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows10.0.22621.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows10.0.22621.0")]
